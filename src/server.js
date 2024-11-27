cfd75cd003d487a7c3f0d4af73b70a124c4f5e76
Maxim Degtiarev <mxim.degt@gmail.com>
2024-11-27 10:36:02 -0300

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import ENV from "./config/enviroment.config.js";
 // DB connection
 import mongoose from "./db/configDB.js";
 
-const url = ENV.URL_BACKEND || "http://localhost:3000";
+const url = ENV.URL_BACKEND || `http://localhost:${ENV.PORT}`;
 
 app.listen(port, () => {
   console.log(`Server running on ${url} 🚀`);
