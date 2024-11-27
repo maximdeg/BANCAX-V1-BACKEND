99238c50127a9451e79609bf87e79eb9925888e6
Maxim Degtiarev <mxim.degt@gmail.com>
2024-11-27 12:09:51 -0300

    email updates

--- src/utils/email.js
+++ src/utils/email.js
@@ -10,62 +10,6 @@ class Email {
     this.from = `Maxim Degtiarev <${ENV.GMAIL_USER}>`;
   }
 
-  //   newTransport() {
-  //     if (process.env.NODE_ENV === "production") {
-  //       // sendgrid
-  //       return nodemailer.createTransport({
-  //         service: "SendGrid",
-  //         auth: {
-  //           user: process.env.SENDGRID_USERNAME,
-  //           pass: process.env.SENDGRID_PASSWORD,
-  //         },
-  //       });
-  //     }
-  //     return nodemailer.createTransport({
-  //       host: process.env.EMAIL_HOST,
-  //       port: process.env.EMAIL_PORT,
-  //       auth: {
-  //         user: process.env.EMAIL_USERNAME,
-  //         pass: process.env.EMAIL_PASSWORD,
-  //       },
-  //       // activate in gmail. 'less secure app' option
-  //     });
-  //   }
-
-  //   // Send the actual email
-  //   async send(template, subject) {
-  //     // Render HTML based on a pug template
-  //     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-  //       firstName: this.firstName,
-  //       url: this.url,
-  //       subject,
-  //     });
-
-  //     // Define the email options
-  //     const mailOptions = {
-  //       from: this.from,
-  //       to: this.to,
-  //       subject,
-  //       html,
-  //       text: convert(html),
-  //     };
-
-  //     // Create a transport and send email
-  //     this.newTransport();
-  //     await this.newTransport().sendMail(mailOptions);
-  //   }
-
-  //   async sendWelcome() {
-  //     await this.send("welcome", "Welcome to the Natourex Family");
-  //   }
-
-  //   async sendPasswordReset() {
-  //     await this.send(
-  //       "passwordReset",
-  //       "Your password reser token (VALID FOR ONLY 10 MIN)"
-  //     );
-  //   }
-
   async sendVerificationToken() {
     try {
       const options = {
@@ -83,6 +27,8 @@ class Email {
     }
   }
 }
+
+export default Email;
 
 // async sendVerificationToken() {
 //   try {
@@ -110,5 +56,58 @@ class Email {
 //   }
 // }
 // }
+//   newTransport() {
+//     if (process.env.NODE_ENV === "production") {
+//       // sendgrid
+//       return nodemailer.createTransport({
+//         service: "SendGrid",
+//         auth: {
+//           user: process.env.SENDGRID_USERNAME,
+//           pass: process.env.SENDGRID_PASSWORD,
+//         },
+//       });
+//     }
+//     return nodemailer.createTransport({
+//       host: process.env.EMAIL_HOST,
+//       port: process.env.EMAIL_PORT,
+//       auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD,
+//       },
+//       // activate in gmail. 'less secure app' option
+//     });
+//   }
 
-export default Email;
+//   // Send the actual email
+//   async send(template, subject) {
+//     // Render HTML based on a pug template
+//     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+//       firstName: this.firstName,
+//       url: this.url,
+//       subject,
+//     });
+
+//     // Define the email options
+//     const mailOptions = {
+//       from: this.from,
+//       to: this.to,
+//       subject,
+//       html,
+//       text: convert(html),
+//     };
+
+//     // Create a transport and send email
+//     this.newTransport();
+//     await this.newTransport().sendMail(mailOptions);
+//   }
+
+//   async sendWelcome() {
+//     await this.send("welcome", "Welcome to the Natourex Family");
+//   }
+
+//   async sendPasswordReset() {
+//     await this.send(
+//       "passwordReset",
+//       "Your password reser token (VALID FOR ONLY 10 MIN)"
+//     );
+//   }
