1eab470a1f0b7265832dad7d5317043e61dc5a27
Maxim Degtiarev <mxim.degt@gmail.com>
2024-12-05 11:17:08 -0300

    create user validations

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,292 +3,293 @@ import jwt from "jsonwebtoken";
 import ENV from "../config/enviroment.config.js";
 import Email from "../utils/email.js";
 import { responseBuilder } from "../utils/builders/responseBuilder.js";
+import { validateCreateUser } from "../utils/errors.handler.js";
 import UserRepository from "../repositories/user.repository.js";
 
-const signToken = id => {
-  return jwt.sign(id, ENV.JWT_SECRET, {
-    expiresIn: ENV.JWT_EXPIRES_IN,
-  });
+const signToken = (id) => {
+    return jwt.sign(id, ENV.JWT_SECRET, {
+        expiresIn: ENV.JWT_EXPIRES_IN,
+    });
 };
 
 // SIGNUP CONTROLLER
 export const createUserController = async (req, res, next) => {
-  try {
-    const { fullname, email, password, password_confirm } = req.body;
-    // TODO: Validations
+    try {
+        const { fullname, email, password, password_confirm } = req.body;
 
-    if (password !== password_confirm) {
-      return res.status(400).json(
-        responseBuilder(false, 400, "BAD_REQUEST", {
-          message: "Passwords do not match",
-        })
-      );
+        const { status_code, response_errors } = validateCreateUser({ fullname, email, password, password_confirm });
+
+        if (status_code > 300) {
+            return res.status(status_code).json(response_errors);
+        }
+
+        const hashed_password = await bcrypt.hash(password, 10);
+
+        const verificationToken = signToken({ email });
+
+        const newUser = {
+            fullname,
+            email,
+            password: hashed_password,
+            password_confirm: hashed_password,
+            verification_token: verificationToken,
+        };
+
+        const url = `${ENV.URL_FRONTEND}/in/verify/${verificationToken}`;
+
+        const user = await UserRepository.saveUser(newUser);
+
+        if (!user) {
+            return res.status(500).json(
+                responseBuilder(false, 500, "SERVER_ERROR", {
+                    location: "createUserController",
+                    message: "Failed to save user",
+                })
+            );
+        }
+
+        await new Email(fullname, email, url).sendVerificationToken();
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "User created successfully", {
+                detail: user,
+            })
+        );
+    } catch (err) {
+        if (err.code === 11000) {
+            res.status(409).json(
+                responseBuilder(true, 409, "DATABASE_ERROR", {
+                    location: "createUserController",
+                    message: "This email already registered",
+                })
+            );
+        } else {
+            res.status(500).json(
+                responseBuilder(true, 500, "SERVER_ERROR", {
+                    location: "createUserController",
+                    message: err.message,
+                })
+            );
+        }
+        return next(err);
     }
-
-    const hashed_password = await bcrypt.hash(password, 10);
-    const verificationToken = signToken({ email });
-
-    const newUser = {
-      fullname,
-      email,
-      password: hashed_password,
-      password_confirm: hashed_password,
-      verification_token: verificationToken,
-    };
-
-    const url = `${ENV.URL_FRONTEND}/in/verify/${verificationToken}`;
-
-    await new Email(fullname, email, url).sendVerificationToken();
-
-    const user = await UserRepository.saveUser(newUser);
-
-    return res.status(200).json(
-      responseBuilder(true, 200, "User created successfully", {
-        detail: user,
-      })
-    );
-  } catch (err) {
-    if (err.code === 11000) {
-      err.message = "Email is already taken.";
-      res.statusCode = 409;
-      res.status(409).json(
-        responseBuilder(true, 409, "DATABASE_ERROR", {
-          location: "createUserController",
-          message: err.message,
-        })
-      );
-    } else {
-      res.status(500).json(
-        responseBuilder(true, 500, "SERVER_ERROR", {
-          location: "createUserController",
-          message: err.message,
-        })
-      );
-    }
-    return next(err);
-  }
 };
 
 // LOGIN CONTROLLER
 export const loginController = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await UserRepository.getByEmail(email);
+        const user = await UserRepository.getByEmail(email);
 
-    if (!user) {
-      return res.status(401).json(
-        responseBuilder(false, 401, "UNAUTHORIZED", {
-          detail: "User does not exist. Please register to continue.",
-        })
-      );
+        if (!user) {
+            return res.status(401).json(
+                responseBuilder(false, 401, "UNAUTHORIZED", {
+                    detail: "User does not exist. Please register to continue.",
+                })
+            );
+        }
+
+        if (!user.email_verified) {
+            return res.status(401).json(
+                responseBuilder(false, 401, "UNAUTHORIZED", {
+                    detail: "User is not verified. Please check your email, including the spam folder, to verify your email.",
+                })
+            );
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            return res.status(401).json(
+                responseBuilder(false, 401, "INVALID_PASSWORD", {
+                    detail: "The password is not correct",
+                })
+            );
+        }
+
+        const token = signToken({
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        });
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "Login successful", {
+                token,
+                user: {
+                    id: user._id,
+                    fullname: user.fullname,
+                    email: user.email,
+                    photo: user.photo,
+                    categories: user.categories,
+                    sources: user.sources,
+                    role: user.role,
+                },
+            })
+        );
+    } catch (err) {
+        res.status(500).json(
+            responseBuilder(false, 500, "SERVER_ERROR", {
+                location: "loginController",
+                message: err.message,
+            })
+        );
     }
-
-    if (!user.email_verified) {
-      return res.status(401).json(
-        responseBuilder(false, 401, "UNAUTHORIZED", {
-          detail:
-            "User is not verified. Please check your email, including the spam folder, to verify your email.",
-        })
-      );
-    }
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-
-    if (!isValidPassword) {
-      return res.status(401).json(
-        responseBuilder(false, 401, "INVALID_PASSWORD", {
-          detail: "The password is not correct",
-        })
-      );
-    }
-
-    const token = signToken({
-      email: user.email,
-      id: user._id,
-      role: user.role,
-    });
-
-    return res.status(200).json(
-      responseBuilder(true, 200, "Login successful", {
-        token,
-        user: {
-          id: user._id,
-          fullname: user.fullname,
-          email: user.email,
-          photo: user.photo,
-          categories: user.categories,
-          sources: user.sources,
-          role: user.role,
-        },
-      })
-    );
-  } catch (err) {
-    res.status(500).json(
-      responseBuilder(false, 500, "SERVER_ERROR", {
-        location: "loginController",
-        message: err.message,
-      })
-    );
-  }
 };
 
 // VERIFY MAIL CONTROLLER
 export const verifyMailValidationTokenController = async (req, res) => {
-  try {
-    const { verification_token } = req.params;
+    try {
+        const { verification_token } = req.params;
 
-    if (!verification_token) {
-      return res.status(400).json(
-        responseBuilder(false, 400, "BAD_REQUEST", {
-          detail: "Invalid verification token",
-        })
-      );
+        if (!verification_token) {
+            return res.status(400).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    detail: "Invalid verification token",
+                })
+            );
+        }
+
+        const decodedUser = jwt.verify(verification_token, ENV.JWT_SECRET);
+
+        const user = await UserRepository.getByEmail(decodedUser.email);
+
+        console.log(user);
+
+        if (!user) throw new Error("USER NOT FOUND");
+
+        // if (user.emailVerified) {
+        //   // verification logic
+        // }
+
+        user.email_verified = true;
+
+        await user.save();
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "SUCCESS", {
+                message: "Email verified successfully",
+                detail: user,
+            })
+        );
+    } catch (err) {
+        res.status(500).json(
+            responseBuilder(false, 500, "SERVER_ERROR", {
+                location: "verifyMailValidationTokenController",
+                message: err.message,
+            })
+        );
     }
-
-    const decodedUser = jwt.verify(verification_token, ENV.JWT_SECRET);
-
-    const user = await UserRepository.getByEmail(decodedUser.email);
-
-    console.log(user);
-
-    if (!user) throw new Error("USER NOT FOUND");
-
-    // if (user.emailVerified) {
-    //   // verification logic
-    // }
-
-    user.email_verified = true;
-
-    await user.save();
-
-    return res.status(200).json(
-      responseBuilder(true, 200, "SUCCESS", {
-        message: "Email verified successfully",
-        detail: user,
-      })
-    );
-  } catch (err) {
-    res.status(500).json(
-      responseBuilder(false, 500, "SERVER_ERROR", {
-        location: "verifyMailValidationTokenController",
-        message: err.message,
-      })
-    );
-  }
 };
 
 // FORGOT PASSWORD CONTROLLER
 export const forgotPasswordController = async (req, res) => {
-  try {
-    const { email } = req.body;
+    try {
+        const { email } = req.body;
 
-    if (!email) {
-      return res.status(401).json(
-        responseBuilder(false, 400, "BAD_REQUEST", {
-          detail: "Email is required",
-        })
-      );
+        if (!email) {
+            return res.status(401).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    detail: "Email is required",
+                })
+            );
+        }
+
+        const user = await UserRepository.getByEmail(email);
+        console.dir(user);
+
+        if (!user) {
+            return res.status(401).json(
+                responseBuilder(false, 404, "UNAUTHORIZED", {
+                    detail: "User not found",
+                })
+            );
+        }
+
+        const token = signToken({
+            email: user.email,
+            id: user._id,
+        });
+
+        const url = `${ENV.URL_FRONTEND}/in/reset-password/${token}`;
+
+        const response = await new Email(user.fullname, user.email, url).sendResetPasswordToken();
+
+        console.log(response);
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "SUCCESS", {
+                message: "Recovery password email sent successfully",
+            })
+        );
+    } catch (err) {
+        res.status(500).json(
+            responseBuilder(false, 500, "SERVER_ERROR", {
+                location: "forgotPasswordController",
+                message: err.message,
+            })
+        );
     }
-
-    const user = await UserRepository.getByEmail(email);
-    console.dir(user);
-
-    if (!user) {
-      return res.status(401).json(
-        responseBuilder(false, 404, "UNAUTHORIZED", {
-          detail: "User not found",
-        })
-      );
-    }
-
-    const token = signToken({
-      email: user.email,
-      id: user._id,
-    });
-
-    const url = `${ENV.URL_FRONTEND}/in/reset-password/${token}`;
-
-    const response = await new Email(
-      user.fullname,
-      user.email,
-      url
-    ).sendResetPasswordToken();
-
-    console.log(response);
-
-    return res.status(200).json(
-      responseBuilder(true, 200, "SUCCESS", {
-        message: "Recovery password email sent successfully",
-      })
-    );
-  } catch (err) {
-    res.status(500).json(
-      responseBuilder(false, 500, "SERVER_ERROR", {
-        location: "forgotPasswordController",
-        message: err.message,
-      })
-    );
-  }
 };
 
 // RESET PASSWORD CONTROLLER
 export const resetPasswordController = async (req, res) => {
-  try {
-    const { password, password_confirm } = req.body.data;
-    const { token } = req.params;
+    try {
+        const { password, password_confirm } = req.body.data;
+        const { token } = req.params;
 
-    if (!password || !password_confirm) {
-      return res.status(401).json(
-        responseBuilder(false, 400, "BAD_REQUEST", {
-          detail: "Password and password confirm are required",
-        })
-      );
+        if (!password || !password_confirm) {
+            return res.status(401).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    detail: "Password and password confirm are required",
+                })
+            );
+        }
+
+        if (password !== password_confirm) {
+            return res.status(401).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    detail: "Passwords do not match",
+                })
+            );
+        }
+
+        const decodedUser = jwt.verify(token, ENV.JWT_SECRET);
+
+        const user = await UserRepository.getByEmail(decodedUser.email);
+
+        if (!user) {
+            return res.status(401).json(
+                responseBuilder(false, 404, "UNAUTHORIZED", {
+                    detail: "User not found",
+                })
+            );
+        }
+
+        const hashed_password = await bcrypt.hash(password, 10);
+
+        user.password = hashed_password;
+        user.password_confirm = hashed_password;
+        user.updated_at = Date.now();
+        user.password_changed_at = Date.now();
+
+        await user.save();
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "SUCCESS", {
+                message: "Password updated successfully",
+                detail: user,
+            })
+        );
+    } catch (err) {
+        res.status(500).json(
+            responseBuilder(false, 500, "SERVER_ERROR", {
+                location: "resetPasswordController",
+                message: err.message,
+            })
+        );
     }
-
-    if (password !== password_confirm) {
-      return res.status(401).json(
-        responseBuilder(false, 400, "BAD_REQUEST", {
-          detail: "Passwords do not match",
-        })
-      );
-    }
-
-    const decodedUser = jwt.verify(token, ENV.JWT_SECRET);
-
-    const user = await UserRepository.getByEmail(decodedUser.email);
-
-    if (!user) {
-      return res.status(401).json(
-        responseBuilder(false, 404, "UNAUTHORIZED", {
-          detail: "User not found",
-        })
-      );
-    }
-
-    const hashed_password = await bcrypt.hash(password, 10);
-
-    user.password = hashed_password;
-    user.password_confirm = hashed_password;
-    user.updated_at = Date.now();
-    user.password_changed_at = Date.now();
-
-    await user.save();
-
-    return res.status(200).json(
-      responseBuilder(true, 200, "SUCCESS", {
-        message: "Password updated successfully",
-        detail: user,
-      })
-    );
-  } catch (err) {
-    res.status(500).json(
-      responseBuilder(false, 500, "SERVER_ERROR", {
-        location: "resetPasswordController",
-        message: err.message,
-      })
-    );
-  }
 };
 
 // UPDATE PASSWORD CONTROLLER
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,96 +2,101 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const sourceSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide a source name."],
-  },
-  color: {
-    type: String,
-  },
-  is_active: {
-    type: Boolean,
-    default: true,
-  },
+    name: {
+        type: String,
+        required: [true, "Please provide a source name."],
+    },
+    color: {
+        type: String,
+    },
+    is_active: {
+        type: Boolean,
+        default: true,
+    },
 });
 
 const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide a category name."],
-  },
-  color: {
-    type: String,
-  },
-  is_active: {
-    type: Boolean,
-    default: true,
-  },
+    name: {
+        type: String,
+        required: [true, "Please provide a category name."],
+    },
+    color: {
+        type: String,
+    },
+    is_active: {
+        type: Boolean,
+        default: true,
+    },
 });
 
 const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: [true, "Please provide your name."],
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide your email."],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email."],
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password."],
-    minlength: 8,
-    // select: false,
-  },
-  password_confirm: {
-    type: String,
-    required: [true, "Please provide a password."],
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: "Passwords are not the same.",
+    fullname: {
+        type: String,
+        required: [true, "Please provide your name."],
+        validate: {
+            validator: function (name) {
+                return name.split(" ").length > 1;
+            },
+            message: "Please provide first name and last name.",
+        },
     },
-  },
-  password_changed_at: {
-    type: Date,
-    default: Date.now(),
-  },
-  email_verified: {
-    type: Boolean,
-    default: false,
-  },
-  photo: {
-    type: String,
-    default: "default.png",
-  },
-  categories: [categorySchema],
-  sources: [sourceSchema],
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  is_active: {
-    type: Boolean,
-    default: true,
-    // select: false,
-  },
-  verification_token: String,
-  created_at: {
-    type: Date,
-    default: Date.now(),
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now(),
-  },
+    email: {
+        type: String,
+        required: [true, "Please provide your email."],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email."],
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password."],
+        minlength: 8,
+        // select: false,
+    },
+    password_confirm: {
+        type: String,
+        required: [true, "Please provide a password."],
+        select: false,
+        validate: {
+            validator: function (pass_confirm) {
+                return pass_confirm === this.password;
+            },
+            message: "Passwords are not the same.",
+        },
+    },
+    password_changed_at: {
+        type: Date,
+        default: Date.now(),
+    },
+    email_verified: {
+        type: Boolean,
+        default: false,
+    },
+    photo: {
+        type: String,
+        default: "default.png",
+    },
+    categories: [categorySchema],
+    sources: [sourceSchema],
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    is_active: {
+        type: Boolean,
+        default: true,
+        // select: false,
+    },
+    verification_token: String,
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now(),
+    },
 });
 
 const User = mongoose.model("User", userSchema);
