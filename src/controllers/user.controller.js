35699d944c5911e9f67dbe04791b15eaa028f0e7
Maxim Degtiarev <mxim.degt@gmail.com>
2024-12-17 17:15:39 -0300

    added get user by id controller and validations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,34 @@ export const updateUserByIdController = async (req, res) => {
     try {
         const user_id = req.params.user_id;
 
+        if (!user_id) {
+            return res.status(400).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    location: "updateUserByIdController",
+                    detail: "User id is required",
+                })
+            );
+        }
+
+        if (!req.body) {
+            return res.status(400).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    location: "updateUserByIdController",
+                    detail: "User data is required",
+                })
+            );
+        }
+
         const user = await UserRepository.updateUser(user_id, req.body);
+
+        if (!user) {
+            return res.status(404).json(
+                responseBuilder(false, 404, "NOT_FOUND", {
+                    location: "updateUserByIdController",
+                    detail: "User not found",
+                })
+            );
+        }
 
         return res.status(200).json(
             responseBuilder(true, 200, "User updated successfully", {
@@ -41,6 +68,55 @@ export const updateUserByIdController = async (req, res) => {
         res.status(500).json(
             responseBuilder(false, 500, "SERVER_ERROR", {
                 location: "updateUserByIdController",
+                detail: err.message,
+            })
+        );
+    }
+};
+
+export const getUserByIdController = async (req, res) => {
+    try {
+        const { user_id } = req.params;
+
+        if (!user_id) {
+            return res.status(400).json(
+                responseBuilder(false, 400, "BAD_REQUEST", {
+                    location: "getUserByIdController",
+                    detail: "User id is required",
+                })
+            );
+        }
+
+        const user = await UserRepository.getById(user_id);
+
+        if (!user) {
+            return res.status(404).json(
+                responseBuilder(false, 404, "NOT_FOUND", {
+                    location: "getUserByIdController",
+                    detail: "User not found",
+                })
+            );
+        }
+
+        return res.status(200).json(
+            responseBuilder(true, 200, "User found successfully", {
+                detail: {
+                    user: {
+                        id: user._id,
+                        fullname: user.fullname,
+                        email: user.email,
+                        photo: user.photo,
+                        categories: user.categories,
+                        sources: user.sources,
+                        role: user.role,
+                    },
+                },
+            })
+        );
+    } catch (err) {
+        res.status(500).json(
+            responseBuilder(false, 500, "SERVER_ERROR", {
+                location: "getUserByIdController",
                 detail: err.message,
             })
         );
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getAllUsersController, updateUserByIdController } from "../controllers/user.controller.js";
+import { getAllUsersController, getUserByIdController, updateUserByIdController } from "../controllers/user.controller.js";
 import { verifyApiKeyMiddleware, verifyTokenMiddleware } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
 
 const userRouter = express.Router();
 
@@ -8,6 +9,7 @@ userRouter.use(verifyApiKeyMiddleware);
 userRouter.use(verifyTokenMiddleware("user"));
 
 userRouter.get("/", getAllUsersController);
+userRouter.get("/:user_id", getUserByIdController);
 userRouter.put("/:user_id", updateUserByIdController);
 
 export default userRouter;
