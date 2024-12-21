4c9d27f06a63fe05254b2eb3c456ba81ec446be2
Maxim Degtiarev <mxim.degt@gmail.com>
2024-12-21 18:22:04 -0300

    Cors credentials updated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ import transactionRouter from "./routes/transaction.router.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://bancax.vercel.app",
+        credentials: true,
+    })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use((req, res, next) => {
