a2f939187491b57081deb8ae61b5b5b29a466bfc
Maxim Degtiarev <mxim.degt@gmail.com>
2024-12-21 18:34:26 -0300

    added more urls to the cors policy

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://bancax.vercel.app",
+        origin: ["https://bancax.vercel.app", 'http://localhost:5173', 'http://localhost:3000'],
         credentials: true,
     })
 );
